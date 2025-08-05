4ca95788ac30f563d3b7b2b08d40ff1bdbfac41c
Felix Lee <leeyoongti@gmail.com>
2025-08-05 23:03:27 +0800

    feat: 🎸 init a new advice-generator project

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Portfolio Turborepo
+
+A collection of applications built with Sveltekit.
+
+Mortgage calculator - https://portfolio-turborepo-mortgage-calcul.vercel.app/
---
+++ apps/advice-generator/src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+</script>
+
+<div class="min-h-screen bg-base-200 flex items-center justify-center p-4">
+	<div
+		class="card lg:card-side bg-base-100 shadow-xl rounded-2xl w-full max-w-5xl"
+	>
+		Hello Advice Generator
+	</div>
+</div>
